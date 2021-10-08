13b8e8d7868a83f261f4087ada2003ed229c3e24
void2412 <void2412@gmail.com>
2021-10-08 14:43:58 +1030

    added convert ui to py script

---
+++ convert_to_py.py
@@ -0,0 +1,8 @@
+import subprocess
+import sys
+uiFile = sys.argv[1]
+pyFile = uiFile.replace(".ui", "") + ".py"
+
+res = subprocess.run("python -m PyQt5.uic.pyuic -x %s -o %s" %(uiFile, pyFile))
+
+print(res)
